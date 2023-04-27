73b85ea97d295732383589dad815350269cd1750
Umoh Andem <andemumoh@gmail.com>
2023-04-27 22:36:43 +0100

    Task 2. Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""A ype-annotated function floor which takes float n 
+as argument and returns the floor of the float.
+"""
+
+
+def floor(n: float) -> int:
+    """Returns the floor of the float"""
+    return int(n)
