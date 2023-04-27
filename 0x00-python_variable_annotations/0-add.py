ee6de1b427e4dbb8da066b37065674dab3066536
Umoh Andem <andemumoh@gmail.com>
2023-04-27 22:30:19 +0100

    Task 0. Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""A type-annotated function add that takes float a and
+float b as arguments and returns their sum as a float.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """Add two floating numbers"""
+    return (a + b)
