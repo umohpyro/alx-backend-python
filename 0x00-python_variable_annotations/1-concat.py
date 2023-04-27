4f13c7bb25e6ef929a598a222728b8d4e6d22cf4
Umoh Andem <andemumoh@gmail.com>
2023-04-27 22:34:32 +0100

    Task 1. Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""A type-annotated function concat that takes string str1 and
+string str2 as arguments and returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenate two strings"""
+    return str1 + str2
