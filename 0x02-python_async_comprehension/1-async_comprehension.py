2c6d41493b74c68ee5d4e5c190b1336302ffc4b1
Umoh Andem <andemumoh@gmail.com>
2023-05-03 14:37:22 +0100

    Task 1. Async Comprehensions

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
-"""Task 1. The coroutine will collect 10 random numbers using an async comprehensing over async_generator, then return the 10 random numbers.
+"""Import async_generator from the previous task and then write a coroutine
+called async_comprehension that takes no arguments.
+
+The coroutine will collect 10 random numbers using an async comprehensing
+over async_generator, then return the 10 random numbers.
 """
 
 
